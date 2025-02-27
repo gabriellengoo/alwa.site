5ffc845c32eda33e61777ed0eeea37613efe22b5
Jnicee <josh@henhouselondon.co.uk>
2025-02-27 12:25:41 +0000

    Update maintenance.js

--- frontend/maintenance.js
+++ frontend/maintenance.js
@@ -1,1 +1,13 @@
+export default function Maintenance() {
+  return (
+    <div style={{
+      textAlign: 'center',
+      marginTop: '100px',
+      fontFamily: 'Arial, sans-serif'
+    }}>
+      <h1>🚧 Site Under Construction 🚧</h1>
+      <p>We’ll be back soon. Thank you for your patience!</p>
+    </div>
+  );
+}
 
